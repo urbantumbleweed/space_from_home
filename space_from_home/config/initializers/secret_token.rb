5b017442ed9683072597850b6296b6c9fd5efcf2
urbantumbleweeed <saunders.stephen@gmail.com>
2014-04-12 14:07:31 -0400

    The first commit wit the rails framework

---
+++ space_from_home/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpaceFromHome::Application.config.secret_key_base = '509bf164f814c3c65e95a00ed5924b38ac65b8f58d17b8bf9cac4275a6a028839f758de4e6095669e869b53bc58c6dfbd584de329ce8c7e98aedb481ff57bda9'
---
+++ space_from_home/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SpaceFromHome::Application.config.session_store :cookie_store, key: '_space_from_home_session'
